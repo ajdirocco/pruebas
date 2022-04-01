440d7bce3931ac68851fd716f7cfd24f996597b7
monna <ajdirocco@gmail.com>
2022-04-01 09:27:39 -0300

    toast

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
+<script>
+	// Follow @kevmodrome on Twitter for more fun Svelte things: https://twitter.com/kevmodrome
+    import notifications from '../stores/notifications.js'
+	import Toast from '$lib/Toast.svelte'
+	
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={() => notifications.info('Wait a INFO...', 100)}>
+	info!
+</button>
+<button on:click={() => notifications.warning('Wait a moment...', 5000)}>
+	Warn!
+</button>
+<button on:click={() => notifications.success('You did it!', 3000)}>
+	Succeed!
+</button>
+<button on:click={() => notifications.danger('STOP NOW!', 500)}>
+	Danger!
+</button>
+<button on:click={() => notifications.default('default', 1000)}>
+	Default!
+</button>
+<Toast />
